9d7af00fd0d62233a23e46cc7019e0a573d9dcb5
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-05-30 18:25:52 +0800

    Updated routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserProfileTemplateComponent } from './user-profile-template/user-profile-template.component';
+import { UserProfileModelComponent } from './user-profile-model/user-profile-model.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'user-profile', component: LoginComponent },
-  { path: 'user-profile-template', component: LoginComponent },
-  { path: 'user-profile-model', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'login' },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'user-profile-template', component: UserProfileTemplateComponent },
+  { path: 'user-profile-model', component: UserProfileModelComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
