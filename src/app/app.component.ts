3eab13b1ca9c0e3fd702bd6913f1c7ffee5c62d1
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-05-30 18:22:24 +0800

    Login form for demonstration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+const profile = {
+  name: {
+    firstname: 'Pankaj',
+    lastname: 'Parkar',
+  },
+  address: {
+    street: 'address 1',
+    zip: '00011',
+    city: 'Mumbai'
+  }
+};
+
 @Component({
   selector: 'lpaf-root',
   templateUrl: './app.component.html',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email;
+  password;
+  check;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form) {
+    console.log(form)
   }
 
 }
