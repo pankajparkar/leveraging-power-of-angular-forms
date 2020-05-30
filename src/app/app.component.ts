daaa29405c6fb5ee763db4b142794080886719eb
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-05-30 17:30:13 +0800

    Added 1st commit CLI

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lpaf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'leveraging-power-of-angular-forms';
+}
